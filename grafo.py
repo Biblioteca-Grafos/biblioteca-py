c4fc7489b1042ef8a0b122ee381c7948f5e520bc
Gabriel Lucas <gabriel2014.lucas@outlook.com>
2024-10-08 22:08:13 -0300

    classe grafo

---
+++ grafo.py
@@ -0,0 +1,43 @@
+class Grafo:
+    def __init__(self):
+        self.grafo = {}
+
+    def adicionarVertice(self, vertice):
+        if vertice not in self.grafo:
+            self.grafo[vertice] = []
+    
+    def mostrarGrafo(self):
+        for vertice in self.grafo:
+            print(f"{vertice}: {self.grafo[vertice]}") 
+
+
+class GrafoNaoDirecionado(Grafo):
+    def adicionarAresta(self, vertice1, vertice2):
+        if vertice1 in self.grafo and vertice2 in self.grafo:
+            self.grafo[vertice1].append(vertice2)
+            self.grafo[vertice2].append(vertice1)
+
+class GrafoDirecionado(Grafo):
+    def adicionarAresta(self, origem, destino):
+        if origem in self.grafo and destino in self.grafo:
+            self.grafo[origem].append(destino)
+
+##Consegui criar um grafo nao direcionado
+def main():
+
+    print("Aqui começa o grafo não direcionado");
+    grafoNDir = GrafoNaoDirecionado();
+    grafoNDir.adicionarVertice("A");
+    grafoNDir.adicionarVertice("B");
+    grafoNDir.adicionarVertice("C");
+    grafoNDir.adicionarAresta("A","B");
+    grafoNDir.adicionarAresta("B","C");
+    print("Mostrando meu grafo")
+    grafoNDir.mostrarGrafo()
+
+    print("Aqui começa o grafo direcionado")
+
+    
+if __name__ == "__main__":
+    main()
+
